eb1d4dfe75a60ca098c9e89d2e966a7402ffca51
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-08-31 10:19:17 +0100

    create inital CreatePost component

---
+++ client/src/CreatePost.js
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+
+const CreatePost = () => {
+    return (
+        <div style={{ width: '90%', margin: 'auto auto', textAlign: 'center' }}>
+            <h1>Create a post</h1>
+            <Button>BACK</Button>
+        </div>
+    );
+};
+
+export default CreatePost;
