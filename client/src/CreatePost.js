0462bbb84097d45149bf3f844ee7ad3cd49a9ae3
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-08-31 10:35:43 +0100

    add form input to state

--- client/src/CreatePost.js
+++ client/src/CreatePost.js
@@ -1,8 +1,28 @@
-import { useNavigate } from 'react-router-dom';
 import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const CreatePost = () => {
     const navigate = useNavigate();
+    const [post, setPost] = useState({
+        title: '',
+        description: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+
+        setPost(prev => {
+            return {
+                ...prev,
+                [name]: value,
+            };
+        })
+    };
+
+    useEffect(() => {
+        console.log(post)
+    }, [post])
 
     return (
         <div style={{ width: '90%', margin: 'auto auto', textAlign: 'center' }}>
@@ -11,13 +31,17 @@ const CreatePost = () => {
                 <Form.Group>
                     <Form.Control
                         name="title"
+                        value={post.title}
                         placeholder="title"
                         style={{ marginBottom: '1rem' }}
+                        onChange={handleChange}
                     />
                     <Form.Control
                         name="description"
+                        value={post.description}
                         placeholder="description"
                         style={{ marginBottom: '1rem' }}
+                        onChange={handleChange}
                     />
                 </Form.Group>
             </Form>
