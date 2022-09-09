8dec4ce345506af7f9299ce39056b9349564af59
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-09-09 16:35:43 +0100

    link home to newly created MVC routes

--- server.js
+++ server.js
@@ -3,23 +3,21 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const connectDB = require('./config/database');
-const instrumentRoutes = require('./routes/instruments')
-const genreRoutes = require('./routes/genres')
+const mainRoutes = require('./routes/main');
+const instrumentRoutes = require('./routes/instruments');
+const genreRoutes = require('./routes/genres');
 
 require('dotenv').config({ path: './config/.env' });
 
 connectDB();
 
-app.use(express.json());``
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Express is here');
-});
-
-app.use('/instruments', instrumentRoutes)
-app.use('/genres', genreRoutes)
+app.use('/', mainRoutes);
+app.use('/instruments', instrumentRoutes);
+app.use('/genres', genreRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
