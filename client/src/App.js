cf2423e20b84cc07b5924cd385af3fe04ae9d5cb
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-09-12 15:42:00 +0100

    add auth controller

--- client/src/App.js
+++ client/src/App.js
@@ -1,102 +1,66 @@
 import './App.css';
-import Instruments from './Instruments';
-import Genres from './Genres';
-import ProfilePic from './images/profile-pic.png';
-import Message from './images/Message.png';
-import Clock from './images/Clock.png';
 
 function App() {
     return (
-        <>
-            <div className="App container">
-                <section className="top-row row mt-5">
-                    <section id="profile-info" className="my-box col-6 profile-card">
-                        <div className="row">
-                            <div className="col-6">
-                                <h2 className="mt-3 fw-bold">Margery Moley</h2>
-                                <p>"Life is a party, let's jam!"</p>
-                                <div className="d-flex justify-content-between pt-3">
-                                    <p className="align-self-end">Bristol, UK</p>
-                                    <div className="last-online d-flex">
-                                        <div className="align-self-center pe-2">
-                                            <img
-                                                src={Message}
-                                                alt="Message Button"
-                                                srcSet=""
-                                                className="pe-5"
-                                            />
-                                            <img
-                                                src={Clock}
-                                                alt="Clock"
-                                            />
-                                        </div>
-                                        <p className="fs-6 text-bottom fw-bold">
-                                            Last active
-                                            <br /> 1 hour ago
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-5">
-                                <img
-                                    src={ProfilePic}
-                                    className="ms-3"
-                                    alt="logo"
-                                    srcSet=""
-                                />
+        <main class="container">
+            <div class="row justify-content-center">
+                <section class="col-6 mt-5">
+                    <form action="/signup" method="POST">
+                        <div class="mb-3">
+                            <label for="userName" class="form-label">
+                                User Name
+                            </label>
+                            <input
+                                type="text"
+                                class="form-control"
+                                id="userName"
+                                name="userName"
+                            />
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">
+                                Email address
+                            </label>
+                            <input
+                                type="email"
+                                class="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                name="email"
+                            />
+                            <div id="emailHelp" class="form-text">
+                                We'll never share your email with anyone else.
                             </div>
                         </div>
-                    </section>
-                    <section id="stats-info" className="my-box col-4 profile-card">
-                        <div className="row">
-                            <div className="col-6">
-                                <h2 className="mt-3 fw-bold">Search Here</h2>
-                                <p>"Life is a party, let's jam!"</p>
-                                <div className="d-flex justify-content-between pt-3">
-                                    <p className="align-self-end">Bristol, UK</p>
-                                </div>
-                            </div>
-                            
+                        <div class="mb-3">
+                            <label for="password" class="form-label">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                class="form-control"
+                                id="password"
+                                name="password"
+                            />
                         </div>
-                    </section>
-                </section>
-
-                <section className="bottom-row row justify-content-between mt-5">
-                    <section id="profile-bio" className="col-4">
-                        <h3>Profile</h3>
-                        <div className="my-box row profile-card">
-                            <h2>Bio</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Libero repudiandae minima
-                                provident aperiam accusamus amet, dolore ut
-                                consectetur sunt officiis!
-                            </p>
-                            <h2>Gear</h2>
-                            <ul className="text-dark">
-                                <li>Martin Dreadnaught</li>
-                                <li>Korg MiniMoog</li>
-                                <li>Korg Vocoder</li>
-                            </ul>
+                        <div class="mb-3">
+                            <label for="confirmPassword" class="form-label">
+                                Confirm Password
+                            </label>
+                            <input
+                                type="password"
+                                class="form-control"
+                                id="confirmPassword"
+                                name="confirmPassword"
+                            />
                         </div>
-                    </section>
-
-                    <section id="instruments" className="col-3">
-                        <h3>Instruments</h3>
-                        <div className="my-box row d-flex">
-                            <Instruments />
-                        </div>
-                    </section>
-
-                    <section id="genres" className="col-3">
-                        <h3>Genres</h3>
-                        <div className="my-box row d-flex">
-                            <Genres />
-                        </div>
-                    </section>
+                        <button type="submit" class="btn btn-primary">
+                            Submit
+                        </button>
+                    </form>
                 </section>
             </div>
-        </>
+        </main>
     );
 }
 
