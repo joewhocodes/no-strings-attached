c7c0e2025f9c6c2acbb21fc5c20259d3766e2fb6
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-12-15 14:40:57 -0500

    add conditional message if user array == 0

--- client/src/components/FriendList.js
+++ client/src/components/FriendList.js
@@ -26,25 +26,32 @@ const FriendList = () => {
             <Header />
             <div className="col-10, col-sm-8, col-md-6 mx-auto">
                 <h1>Friends</h1>
-                <table className="table table-striped table-bordered table-hover mt-3">
-                    <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Main Instrument</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {users
-                        .filter(e => loggedInUser.friends.includes(e._id))
-                        .map((user) => (
-                                <tr key = {user._id}>
-                                    <td><NavLink to={`/users/${user._id}`}>{user.firstName}</NavLink></td>
-                                    <td>{user.instruments > 0 && Object.keys(user.instruments[0])}</td>
+                {loggedInUser.friends.length === 0 ? 
+                        <h3>No friends...yet. Don't worry, get on the users page and find some!</h3>
+                    :
+                        <table className="table table-striped table-bordered table-hover mt-3">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Main Instrument</th>
                                 </tr>
-                            ))
-                    }
-                    </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+
+                            {users
+                                .filter(e => loggedInUser.friends.includes(e._id))
+                                .map((user) => (
+                                        <tr key = {user._id}>
+                                            <td><NavLink to={`/users/${user._id}`}>{user.firstName}</NavLink></td>
+                                            <td>{user.instruments > 0 && Object.keys(user.instruments[0])}</td>
+                                        </tr>
+                                    ))
+                            }
+                            
+
+                            </tbody>
+                        </table>
+                }
             </div>
         </>
     );
