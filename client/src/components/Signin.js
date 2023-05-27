eaf1ea335a0ae7b3790e9c5959259856b36a4d15
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-05-27 10:08:01 +0100

    complete integration of chakra with signin component

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,36 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useFormik, Form } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { signinUser } from './stateSlices/signinSlice';
 import logo from '../images/logo.png';
 import {
-    FormErrorMessage,
-    FormHelperText,
     Box,
-    VStack,
     Flex,
     FormControl,
     FormLabel,
     Input,
-    InputGroup,
-    HStack,
-    InputRightElement,
     Stack,
     Button,
     Heading,
     Text,
     useColorModeValue,
     Link,
-    Checkbox,
-    StackDivider
-  } from '@chakra-ui/react'
-  import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+} from '@chakra-ui/react';
 
 const Signin = () => {
-    const { status, loggedInUser, error } = useSelector((state) => state.signin);
-    const [showPassword, setShowPassword] = useState(false);
+    const { status, loggedInUser, error } = useSelector(state => state.signin);
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -46,9 +36,8 @@ const Signin = () => {
                 .required('Please enter your email address'),
             password: Yup.string().required('Please enter your password'),
         }),
-        onSubmit: async (values) => {
+        onSubmit: async values => {
             dispatch(signinUser(values));
-            console.log(values)
         },
     });
 
@@ -59,118 +48,85 @@ const Signin = () => {
     }, [loggedInUser, navigate]);
 
     return (
-            <Flex
-                minH={'85vh'}
-                align={'center'}
-                justify={'center'}
-                direction={'column'}
-            >
-                <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Flex
+            minH={'80vh'}
+            align={'center'}
+            justify={'center'}
+            direction={'column'}>
+            <Stack spacing={10} mx={'auto'} maxW={'lg'} py={12} px={6}>
                 <img src={logo} alt='Logo' />
-                    <Stack align={'center'}>
-                        <Heading fontSize={'4xl'}>
-                            Sign in to your account
-                        </Heading>
-                        <Text fontSize={'lg'} color={'gray.600'}>
-                            And get on the jam ✌️
-                        </Text>
-                    </Stack>
-                    <form onSubmit={formik.handleSubmit}>
-                        <Box
-                            rounded={'lg'}
-                            bg={useColorModeValue('white', 'gray.700')}
-                            boxShadow={'lg'}
-                            p={8}>
-                            <Stack spacing={4}>
-                                <FormControl id='email'>
-                                    <FormLabel>Email address</FormLabel>
-                                    <Input type='email' 
-                                        {...formik.getFieldProps('email')}
-                                    />
-                                </FormControl>
-                                <FormControl id='password'>
-                                    <FormLabel>Password</FormLabel>
-                                    <Input type='password' 
-                                        {...formik.getFieldProps('password')}
-                                    />
-                                </FormControl>
-                                <Stack spacing={10}>
-                                    <Stack
-                                        direction={{ base: 'column', sm: 'row' }}
-                                        align={'start'}
-                                        justify={'space-between'}></Stack>
-                                    <Button
-                                        type='submit'
-                                        bg={'blue.400'}
-                                        color={'white'}
-                                        _hover={{
-                                            bg: 'blue.500',
-                                        }}>
-                                        Sign in
-                                    </Button>
-                                </Stack>
-                            </Stack>
-                        </Box>
-                    </form>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        And get on the jam ✌️
+                    </Text>
                 </Stack>
-            </Flex>
-            /* <Box>
-                <img src={logo} width='400px' alt='Logo' />
-                <Heading>Login</Heading>
                 <form onSubmit={formik.handleSubmit}>
-                    <div>
-                        {error && (
-                            <div className='alert alert-danger' role='alert'>
-                                {error}
-                            </div>
-                        )}
-                        <label htmlFor='email'>Email</label>
-                        <input
-                            className='form-control form-control-lg'
-                            id='email'
-                            name='email'
-                            type='email'
-                            {...formik.getFieldProps('email')}
-                        />
-                        {formik.touched.email && formik.errors.email ? (
-                            <small className='form-text text-danger'>
-                                {formik.errors.email}
-                            </small>
-                        ) : null}
-                    </div>
-                    <div className=''>
-                        <label htmlFor='password'>Password</label>
-                        <input
-                            className='form-control form-control-lg'
-                            id='password'
-                            name='password'
-                            type='password'
-                            {...formik.getFieldProps('password')}
-                        />
-                        {formik.touched.password && formik.errors.password ? (
-                            <small className='form-text text-danger'>
-                                {formik.errors.password}
-                            </small>
-                        ) : null}
-                    </div>
-                    <Button type='submit' size='lg' mt='5' colorScheme='teal'>
-                        {status === 'loading' ? (
-                            <div
-                                className='spinner-border text-light'
-                                role='status'>
-                                <span className='sr-only'></span>
-                            </div>
-                        ) : null}{' '}
-                        Let's Go!
-                    </Button>
+                    {error && (
+                        <div className='alert alert-danger' role='alert'>
+                            {error}
+                        </div>
+                    )}
+                    <Box
+                        rounded={'lg'}
+                        bg={useColorModeValue('white', 'gray.700')}
+                        boxShadow={'lg'}
+                        p={8}>
+                        <Stack spacing={5}>
+                            <FormControl id='email'>
+                                <FormLabel>Email address</FormLabel>
+                                <Input
+                                    type='email'
+                                    {...formik.getFieldProps('email')}
+                                />
+                                {formik.touched.email && formik.errors.email ? (
+                                    <small className='form-text text-danger'>
+                                        {formik.errors.email}
+                                    </small>
+                                ) : null}
+                            </FormControl>
+                            <FormControl id='password'>
+                                <FormLabel>Password</FormLabel>
+                                <Input
+                                    type='password'
+                                    {...formik.getFieldProps('password')}
+                                />
+                                {formik.touched.password &&
+                                formik.errors.password ? (
+                                    <small className='form-text text-danger'>
+                                        {formik.errors.password}
+                                    </small>
+                                ) : null}
+                            </FormControl>
+                            <Button
+                                type='submit'
+                                bg={'blue.400'}
+                                color={'white'}
+                                _hover={{
+                                    bg: 'blue.500',
+                                }}>
+                                {status === 'loading' ? (
+                                    <div
+                                        className='spinner-border text-light'
+                                        role='status'>
+                                        <span className='sr-only'></span>
+                                    </div>
+                                ) : null}{' '}
+                                Sign in
+                            </Button>
+                        </Stack>
+                    </Box>
                 </form>
-                <Box mx='auto'>
-                    <Heading mt='5'>Don't have an account yet?</Heading>
-                    <Button colorScheme='teal'>
-                        <NavLink to={'/'}>Sign Up Here</NavLink>
-                    </Button>
-                </Box>
-            </Box> */
+                <Stack pt={6}>
+                    <Text align={'center'}>
+                        Not yet a user?{' '}
+                        <NavLink to={'/'}>
+                            <Link color={'blue.400'}>Sign Up</Link>
+                        </NavLink>
+                    </Text>
+                </Stack>
+            </Stack>
+        </Flex>
     );
 };
 
