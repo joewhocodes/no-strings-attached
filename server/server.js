abfe36c3c8d265830e0d49343a884a5a32ed2fd7
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-04 21:44:55 +0200

    update server.js

--- server/server.js
+++ server/server.js
@@ -15,13 +15,6 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/build'));
-
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-}
-
 // Bodyparser middleware
 app.use(
     bodyParser.urlencoded({
